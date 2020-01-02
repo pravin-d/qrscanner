de52376aaa210d62de192de9f52c8cc47f03e90e
Pravin D <probablypravin@gmail.com>
2020-01-03 00:11:14 +0530

    first commit

---
+++ build/precache-manifest.34188fa259e8f91f0e6588ff1151da18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4edea86ae79cf261879e9217504549a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "21bfe51edebc29969bf0",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ed6ade7feebc8355cbff",
+    "url": "./static/js/2.361c2882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.361c2882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21bfe51edebc29969bf0",
+    "url": "./static/js/main.21080302.chunk.js"
+  },
+  {
+    "revision": "f755f22bfda9db03e406",
+    "url": "./static/js/runtime-main.3a8628ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ build/precache-manifest.4971379583963bc6b7506dfa700311cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c621d6f632f22d042e55b8fc984bda9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eb0680fe6eed7baacdfc",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ed6ade7feebc8355cbff",
+    "url": "./static/js/2.361c2882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.361c2882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb0680fe6eed7baacdfc",
+    "url": "./static/js/main.1f980b40.chunk.js"
+  },
+  {
+    "revision": "f755f22bfda9db03e406",
+    "url": "./static/js/runtime-main.3a8628ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
